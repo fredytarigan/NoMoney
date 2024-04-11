27cd8f5129d2d37a80d30606f6fc6dd35072cd59
Fredy Samuel B Tarigan <fredy.tarigan@tiptip.tv>
2024-04-11 15:17:14 +0700

    add test for update families by id

--- tests/families.rs
+++ tests/families.rs
@@ -62,12 +62,12 @@ fn test_create_families() {
 
 #[test]
 fn test_view_families() {
-    // Setup
+    /* Setup Section */
     let client = setup_client();
     let families = create_test_families(&client);
     let uid = families["data"]["id"].as_str().unwrap();
 
-    // Test
+    /* Test Section */
     let response = client
         .get(format!("{}/families/{}", APP_HOST, uid))
         .send()
@@ -102,5 +102,59 @@ fn test_view_families() {
 
     assert_eq!(response.status(), StatusCode::NOT_FOUND);
 
+    /* Cleanup Section */
+    delete_test_families(&client, families);
+}
+
+#[test]
+fn test_update_families() {
+    /* Setup Section */
+    let client = setup_client();
+    let families = create_test_families(&client);
+    let uid = families["data"]["id"].as_str().unwrap();
+
+    /* Test Section */
+    let response = client
+        .put(format!("{}/families/{}", APP_HOST, uid))
+        .json(&json!({
+            "name": "Modified Family Name",
+            "description": "Modified Family Description"
+        }))
+        .send()
+        .unwrap();
+
+    // response test
+    // should be 200
+    assert_eq!(response.status(), StatusCode::OK);
+
+    // data test
+    // left side should matched right side
+    let families: Value = response.json().unwrap();
+    assert_eq!(
+        families["data"],
+        json!({
+            "id": families["data"]["id"],
+            "name": "Modified Family Name",
+            "description": "Modified Family Description",
+            "created_at": families["data"]["created_at"],
+            "updated_at": families["data"]["updated_at"],
+        })
+    );
+
+    // try update data with dummy id
+    // test should be failed with 404
+    let dummy_uuid = uuid::Uuid::new_v4();
+    let response = client
+        .put(format!("{}/families/{}", APP_HOST, dummy_uuid))
+        .json(&json!({
+            "name": "Modified Family Name",
+            "description": "Modified Family Description"
+        }))
+        .send()
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::NOT_FOUND);
+
+    /* Cleanup Section */
     delete_test_families(&client, families);
 }
