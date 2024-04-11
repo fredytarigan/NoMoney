d16952c5cc9749224dd155866c3d1aecc7413641
Fredy Samuel B Tarigan <fredy.tarigan@tiptip.tv>
2024-04-11 15:02:05 +0700

    add test for create families

--- tests/families.rs
+++ tests/families.rs
@@ -1,7 +1,7 @@
 pub mod setup;
 
 use reqwest::StatusCode;
-use serde_json::Value;
+use serde_json::{json, Value};
 use setup::{setup_client, APP_HOST};
 
 use crate::setup::families::delete_test_families;
@@ -23,3 +23,38 @@ fn test_get_families() {
     // Cleanup
     delete_test_families(&client, families);
 }
+
+#[test]
+fn test_create_families() {
+    // Setup
+    let client = setup_client();
+    let response = client
+        .post(format!("{}/families", APP_HOST))
+        .json(&json!(
+            {
+                "name": "Test Family Name",
+                "description": "Test Family Description"
+            }
+        ))
+        .send()
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::CREATED);
+
+    let families: Value = response.json().unwrap();
+
+    assert_eq!(
+        families["data"],
+        json!({
+            "id": families["data"]["id"],
+            "name": "Test Family Name",
+            "description": "Test Family Description",
+            "created_at": families["data"]["created_at"],
+            "updated_at": families["data"]["updated_at"],
+        })
+    );
+
+    // Cleanup
+    delete_test_families(&client, families);
+}
+
