e1e90d241e46c872bb0c4a1482c1de7dd9335919
Fredy Samuel B Tarigan <fredy.tarigan@tiptip.tv>
2024-04-11 15:26:47 +0700

    add comment in test function

--- tests/families.rs
+++ tests/families.rs
@@ -8,28 +8,44 @@ use crate::setup::families::delete_test_families;
 
 #[test]
 fn test_get_families() {
-    // Setup
+    /*
+        Setup Section
+    */
     let client = setup_client();
     let families = setup::families::create_test_families(&client);
 
-    // Test
+    /*
+       Test Section
+    */
+    // response test
     let response = client.get(format!("{}/families", APP_HOST)).send().unwrap();
+
+    // response should be 200
     assert_eq!(response.status(), StatusCode::OK);
 
+    // data test
     let json: Value = response.json().unwrap();
 
+    // left side must matched right side
     assert!(json["data"].as_array().unwrap().contains(&families["data"]));
 
-    // Cleanup
+    /*
+       Cleanup Section
+    */
     delete_test_families(&client, families);
 }
 
 #[test]
 fn test_create_families() {
-    // Setup
+    /*
+        Setup Section
+    */
     let client = setup_client();
 
-    // Test
+    /*
+        Test Section
+    */
+    // response test
     let response = client
         .post(format!("{}/families", APP_HOST))
         .json(&json!(
@@ -41,45 +57,12 @@ fn test_create_families() {
         .send()
         .unwrap();
 
+    // response should be 201
     assert_eq!(response.status(), StatusCode::CREATED);
 
-    let families: Value = response.json().unwrap();
-
-    assert_eq!(
-        families["data"],
-        json!({
-            "id": families["data"]["id"],
-            "name": "Test Family Name",
-            "description": "Test Family Description",
-            "created_at": families["data"]["created_at"],
-            "updated_at": families["data"]["updated_at"],
-        })
-    );
-
-    // Cleanup
-    delete_test_families(&client, families);
-}
-
-#[test]
-fn test_view_families() {
-    /* Setup Section */
-    let client = setup_client();
-    let families = create_test_families(&client);
-    let uid = families["data"]["id"].as_str().unwrap();
-
-    /* Test Section */
-    let response = client
-        .get(format!("{}/families/{}", APP_HOST, uid))
-        .send()
-        .unwrap();
-
-    // response test
-    // should be 200
-    assert_eq!(response.status(), StatusCode::OK);
-
-    let families: Value = response.json().unwrap();
-
     // data test
+    let families: Value = response.json().unwrap();
+
     // left side must matched right side
     assert_eq!(
         families["data"],
@@ -92,28 +75,77 @@ fn test_view_families() {
         })
     );
 
-    // test response code when invalid ID is provided
-    // should return 404
+    /*
+        Cleanup Section
+    */
+    delete_test_families(&client, families);
+}
+
+#[test]
+fn test_view_families() {
+    /*
+        Setup Section
+    */
+    let client = setup_client();
+    let families = create_test_families(&client);
+    let uid = families["data"]["id"].as_str().unwrap();
+
+    /*
+        Test Section
+    */
+    // response test
+    let response = client
+        .get(format!("{}/families/{}", APP_HOST, uid))
+        .send()
+        .unwrap();
+
+    // response should be 200
+    assert_eq!(response.status(), StatusCode::OK);
+
+    // data test
+    let families: Value = response.json().unwrap();
+
+    // left side must matched right side
+    assert_eq!(
+        families["data"],
+        json!({
+            "id": families["data"]["id"],
+            "name": "Test Family Name",
+            "description": "Test Family Description",
+            "created_at": families["data"]["created_at"],
+            "updated_at": families["data"]["updated_at"],
+        })
+    );
+
+    // test view data with dummy id
     let dummy_uuid = uuid::Uuid::new_v4();
     let response = client
         .get(format!("{}/families/{}", APP_HOST, dummy_uuid))
         .send()
         .unwrap();
 
+    // response should be 404
     assert_eq!(response.status(), StatusCode::NOT_FOUND);
 
-    /* Cleanup Section */
+    /*
+        Cleanup Section
+    */
     delete_test_families(&client, families);
 }
 
 #[test]
 fn test_update_families() {
-    /* Setup Section */
+    /*
+        Setup Section
+    */
     let client = setup_client();
     let families = create_test_families(&client);
     let uid = families["data"]["id"].as_str().unwrap();
 
-    /* Test Section */
+    /*
+        Test Section
+    */
+    // response test
     let response = client
         .put(format!("{}/families/{}", APP_HOST, uid))
         .json(&json!({
@@ -123,13 +155,13 @@ fn test_update_families() {
         .send()
         .unwrap();
 
-    // response test
-    // should be 200
+    // response should be 200
     assert_eq!(response.status(), StatusCode::OK);
 
     // data test
-    // left side should matched right side
     let families: Value = response.json().unwrap();
+
+    // left side should matched right side
     assert_eq!(
         families["data"],
         json!({
@@ -141,8 +173,7 @@ fn test_update_families() {
         })
     );
 
-    // try update data with dummy id
-    // test should be failed with 404
+    // test update data with dummy id
     let dummy_uuid = uuid::Uuid::new_v4();
     let response = client
         .put(format!("{}/families/{}", APP_HOST, dummy_uuid))
@@ -153,8 +184,43 @@ fn test_update_families() {
         .send()
         .unwrap();
 
+    // response should be 404
     assert_eq!(response.status(), StatusCode::NOT_FOUND);
 
-    /* Cleanup Section */
+    /*
+        Cleanup Section
+    */
     delete_test_families(&client, families);
+}
+
+#[test]
+fn test_delete_families() {
+    /*
+        Setup Section
+    */
+    let client = setup_client();
+    let families = create_test_families(&client);
+    let uid = families["data"]["id"].as_str().unwrap();
+
+    /*
+        Test Section
+    */
+    // response test
+    let response = client
+        .delete(format!("{}/families/{}", APP_HOST, uid))
+        .send()
+        .unwrap();
+
+    // response should be 204
+    assert_eq!(response.status(), StatusCode::NO_CONTENT);
+
+    // test delete data with dummy id
+    let dummy_uuid = uuid::Uuid::new_v4();
+    let response = client
+        .delete(format!("{}/families/{}", APP_HOST, dummy_uuid))
+        .send()
+        .unwrap();
+
+    // response should be 204
+    assert_eq!(response.status(), StatusCode::NO_CONTENT);
 }
