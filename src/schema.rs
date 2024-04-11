4b4c51206923c7b790511981af70a0c971357c84
Fredy Samuel B Tarigan <fredy.tarigan@tiptip.tv>
2024-04-11 15:36:53 +0700

    add migration for roles

--- src/schema.rs
+++ src/schema.rs
@@ -10,3 +10,19 @@ diesel::table! {
         updated_at -> Timestamp,
     }
 }
+
+diesel::table! {
+    roles (id) {
+        id -> Uuid,
+        #[max_length = 64]
+        name -> Varchar,
+        description -> Text,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    families,
+    roles,
+);
